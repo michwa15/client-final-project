fdbd3ce4e6be21bf23928bb8f8e4c475bd5abb2a
Michael Wachsman <michaelwac@wix.com>
2022-08-08 10:54:25 +0300

    pr

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
-import {SignIn} from './components/sign-in/SignIn'
+import {SignIn} from './components/sign-in/SignIn';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   const [data, setData] = React.useState(null);
 
   React.useEffect(() => {
+    
     fetch("/api")
       .then((res) => res.json())
       .then((data) => setData(data.message));
   }, []);
 
+  const Data = () => {
+    
+    useEffect(() => {
+      fetch("/prod")
+    })
+
+    return (
+      <div>{data}</div>
+    )
+  }
+
   return (
-    <div className="App">
-      <SignIn />
-    </div>
+        <Routes>
+          <Route path="/" element={<SignIn />} />
+          <Route path="api" element={<Data />} />
+        </Routes>
+    
+      // <div>{data ? data : 'not ready...'}</div>
+      // <div>
+      //   <SignIn />
+      // </div>
+      
   );
 }
 
